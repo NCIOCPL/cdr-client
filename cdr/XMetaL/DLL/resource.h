89a732e250169515e4158a1754e06229a502e7a2
Bob Kline <bkline@rksystems.com>
2001-06-09 12:39:23 +0000

    <R464> Added dialog box for logon progress window.

--- cdr/XMetaL/DLL/resource.h
+++ cdr/XMetaL/DLL/resource.h
@@ -18,17 +18,28 @@
 #define IDC_RADIO3                      206
 #define IDD_DIALOG6                     206
 #define IDC_BUTTON1                     207
+#define IDD_DIALOG7                     207
 #define IDC_COMBO1                      208
+#define IDD_DIALOG8                     208
 #define IDC_CHECK1                      209
 #define IDC_CHECK2                      210
+#define IDC_CHECK3                      213
+#define IDC_CHECK4                      214
+#define IDC_CHECK5                      215
+#define IDC_BUTTON2                     216
+#define IDC_PROGRESS1                   217
+#define ID_GENERAL_PROGRESS_TEXT        218
+#define ID_CURRENT_ACTIVITY_TEXT        219
+#define IDC_LOGON_PROGRESS_TEXT         220
+#define ID_LOGON_OK                     221
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        207
+#define _APS_NEXT_RESOURCE_VALUE        209
 #define _APS_NEXT_COMMAND_VALUE         32768
-#define _APS_NEXT_CONTROL_VALUE         212
+#define _APS_NEXT_CONTROL_VALUE         222
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
