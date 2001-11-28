3ab8b265352ce68e252879b8ea8bb1cc7443f649
Bob Kline <bkline@rksystems.com>
2001-11-28 21:13:23 +0000

    <R840> Removed some extra variables and casting for DOM objects.

--- cdr/XMetaL/DLL/SchemaPicklist.cpp
+++ cdr/XMetaL/DLL/SchemaPicklist.cpp
@@ -19,28 +19,28 @@ CSchemaPicklist::CSchemaPicklist(const cdr::StringList* vvSet_,
                                  const CString& curVal_,
                                  const CString& elemName_,
                                  CWnd* pParent /*=NULL*/)
-	: CDialog(CSchemaPicklist::IDD, pParent), vvSet(vvSet_),
+    : CDialog(CSchemaPicklist::IDD, pParent), vvSet(vvSet_),
       curVal(curVal_), elemName(elemName_)
 {
-	//{{AFX_DATA_INIT(CSchemaPicklist)
-		// NOTE: the ClassWizard will add member initialization here
-	//}}AFX_DATA_INIT
+    //{{AFX_DATA_INIT(CSchemaPicklist)
+        // NOTE: the ClassWizard will add member initialization here
+    //}}AFX_DATA_INIT
 }
 
 
 void CSchemaPicklist::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CSchemaPicklist)
-	DDX_Control(pDX, IDC_LIST1, m_listBox);
-	//}}AFX_DATA_MAP
+    CDialog::DoDataExchange(pDX);
+    //{{AFX_DATA_MAP(CSchemaPicklist)
+    DDX_Control(pDX, IDC_LIST1, m_listBox);
+    //}}AFX_DATA_MAP
 }
 
 
 BEGIN_MESSAGE_MAP(CSchemaPicklist, CDialog)
-	//{{AFX_MSG_MAP(CSchemaPicklist)
-	ON_LBN_DBLCLK(IDC_LIST1, OnDblclkList1)
-	//}}AFX_MSG_MAP
+    //{{AFX_MSG_MAP(CSchemaPicklist)
+    ON_LBN_DBLCLK(IDC_LIST1, OnDblclkList1)
+    //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -48,49 +48,69 @@ END_MESSAGE_MAP()
 
 void CSchemaPicklist::OnDblclkList1() 
 {
-    OnOK();	
+    OnOK();    
 }
 
 void CSchemaPicklist::OnOK() 
 {
-	int curSel = m_listBox.GetCurSel();
+    int curSel = m_listBox.GetCurSel();
     if (curSel >= 0) {
         CWaitCursor wc;
         CString str;
         m_listBox.GetText(curSel, str);
         ::Selection selection = cdr::getApp().GetSelection();
         selection.SetReadOnlyContainer(FALSE);
-        ::DOMNode node = selection.GetContainerNode();
-        while (node && node.GetNodeType() != 1) // DOMElement
-            node = node.GetParentNode();
-        if (node) {
-            ::DOMElement elem(node);
-            elem.m_bAutoRelease = 0;
-            ::DOMNode child = node.GetFirstChild();
-            while (child && child.GetNodeType() != 3) // DOMText
-                child = child.GetNextSibling();
-            if (child) {
-                ::DOMText textNode(child);
-                textNode.m_bAutoRelease = 0;
-                long len = textNode.GetLength();
-                textNode.replaceData(0, len, str);
+        ::DOMElement elem = selection.GetContainerNode();
+        while (elem && elem.GetNodeType() != 1) // DOMElement
+            elem = elem.GetParentNode();
+        if (elem) {
+            bool found = false;
+            ::DOMText textNode = elem.GetFirstChild();
+
+            // See warning in Command.cpp for doInsertLink().
+            while (textNode) {
+                ::DOMText nextNode = textNode.GetNextSibling();
+                if (textNode.GetNodeType() == 3) { // DOMText
+                    textNode.SetData(str);
+                    str = L"";
+                    found = true;
+                }
+#if 0           // Try this in the macro file; it isn't working here
+                // (problem turned out to be a SoftQuad bug, fixed in 2.1
+                else if (textNode.GetNodeType() == 7) { // PI
+                    ::DOMProcessingInstruction pi(textNode);
+                    ::AfxMessageBox(L"got PI" + pi.GetTarget());
+                    if (pi.GetTarget() == _T("xm-replace_text")) {
+                        elem.removeChild(pi);
+                    }
+                    else
+                        pi.m_bAutoRelease = 0;
+                }
+#endif
+                textNode = nextNode;
+            }
+            if (!found) {
+                ::_Document curDoc = cdr::getApp().GetActiveDocument();
+                ::DOMText newNode = curDoc.createTextNode(str);
+                elem.appendChild(newNode);
+                newNode.m_lpDispatch->Release(); // XXX Surprise!  We need this!
             }
         }
         selection.SetReadOnlyContainer(TRUE);
-        EndDialog(IDCANCEL);
+        EndDialog(IDOK);
     }
 }
 
 BOOL CSchemaPicklist::OnInitDialog() 
 {
-	CDialog::OnInitDialog();
-    SetWindowText(_T("Valid Values for Element ") + elemName);	
+    CDialog::OnInitDialog();
+    SetWindowText(_T("Valid Values for Element ") + elemName);    
     cdr::StringList::const_iterator i = vvSet->begin();
     while (i != vvSet->end())
         m_listBox.AddString(*i++);
     if (!curVal.IsEmpty())
         m_listBox.SelectString(-1, curVal);
-	
-	return TRUE;  // return TRUE unless you set the focus to a control
-	              // EXCEPTION: OCX Property Pages should return FALSE
+    
+    return TRUE;  // return TRUE unless you set the focus to a control
+                  // EXCEPTION: OCX Property Pages should return FALSE
 }
