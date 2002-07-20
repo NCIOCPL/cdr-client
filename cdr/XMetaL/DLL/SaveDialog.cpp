7913d261278d8740a463d7a33a24babf136f1562
Bob Kline <bkline@rksystems.com>
2002-07-20 13:45:43 +0000

    <R2156> Added automatic setting of dialog controls.

--- cdr/XMetaL/DLL/SaveDialog.cpp
+++ cdr/XMetaL/DLL/SaveDialog.cpp
@@ -1,9 +1,12 @@
 /*
- * $Id: SaveDialog.cpp,v 1.3 2001-11-28 21:05:11 bkline Exp $
+ * $Id: SaveDialog.cpp,v 1.4 2002-07-20 13:45:43 bkline Exp $
  *
  * Implementation of dialog object for performing a CDR document search.
  *
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2001/11/28 21:05:11  bkline
+ * Added m_readyForReview member variable.
+ *
  * Revision 1.2  2001/06/09 12:42:17  bkline
  * Removed title control; added controls for versioning and status.
  *
@@ -28,40 +31,69 @@ static char THIS_FILE[] = __FILE__;
 
 
 CSaveDialog::CSaveDialog(CWnd* pParent /*=NULL*/)
-	: CDialog(CSaveDialog::IDD, pParent)
+    : CDialog(CSaveDialog::IDD, pParent)
 {
-	//{{AFX_DATA_INIT(CSaveDialog)
-	m_validate = FALSE;
-	m_checkIn = FALSE;
-	m_comment = _T("");
-	m_createVersion = FALSE;
-	m_versionPublishable = FALSE;
-	m_docInactive = FALSE;
-	m_readyForReview = FALSE;
-	//}}AFX_DATA_INIT
+    //{{AFX_DATA_INIT(CSaveDialog)
+    m_validate = FALSE;
+    m_checkIn = FALSE;
+    m_comment = _T("");
+    m_createVersion = FALSE;
+    m_versionPublishable = FALSE;
+    m_docInactive = FALSE;
+    m_readyForReview = FALSE;
+    //}}AFX_DATA_INIT
 }
 
 
 void CSaveDialog::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CSaveDialog)
-	DDX_Check(pDX, IDC_CHECK1, m_validate);
-	DDX_Check(pDX, IDC_CHECK2, m_checkIn);
-	DDX_Text(pDX, IDC_EDIT2, m_comment);
-	DDV_MaxChars(pDX, m_comment, 4000);
-	DDX_Check(pDX, IDC_CHECK3, m_createVersion);
-	DDX_Check(pDX, IDC_CHECK4, m_versionPublishable);
-	DDX_Check(pDX, IDC_CHECK5, m_docInactive);
-	DDX_Check(pDX, IDC_CHECK6, m_readyForReview);
-	//}}AFX_DATA_MAP
+    CDialog::DoDataExchange(pDX);
+    //{{AFX_DATA_MAP(CSaveDialog)
+    DDX_Check(pDX, IDC_CHECK1, m_validate);
+    DDX_Check(pDX, IDC_CHECK2, m_checkIn);
+    DDX_Text(pDX, IDC_EDIT2, m_comment);
+    DDV_MaxChars(pDX, m_comment, 4000);
+    DDX_Check(pDX, IDC_CHECK3, m_createVersion);
+    DDX_Check(pDX, IDC_CHECK4, m_versionPublishable);
+    DDX_Check(pDX, IDC_CHECK5, m_docInactive);
+    DDX_Check(pDX, IDC_CHECK6, m_readyForReview);
+    //}}AFX_DATA_MAP
 }
 
 
 BEGIN_MESSAGE_MAP(CSaveDialog, CDialog)
-	//{{AFX_MSG_MAP(CSaveDialog)
-	//}}AFX_MSG_MAP
+    //{{AFX_MSG_MAP(CSaveDialog)
+    //}}AFX_MSG_MAP
+    ON_BN_CLICKED(IDC_CHECK4, OnBnClickedCheck4)
+    ON_BN_CLICKED(IDC_CHECK3, OnBnClickedCheck3)
+    ON_BN_CLICKED(IDC_CHECK1, OnBnClickedCheck1)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CSaveDialog message handlers
+
+void CSaveDialog::OnBnClickedCheck4()
+{
+    fixCheckBoxes();
+}
+
+void CSaveDialog::OnBnClickedCheck3()
+{
+    fixCheckBoxes();
+}
+
+void CSaveDialog::OnBnClickedCheck1()
+{
+    fixCheckBoxes();
+}
+
+void CSaveDialog::fixCheckBoxes()
+{
+    UpdateData(TRUE);
+    if (m_versionPublishable &&
+        (!m_createVersion || !m_validate)) {
+        m_createVersion = TRUE;
+        m_validate = TRUE;
+        UpdateData(FALSE);
+    }
+}
--- cdr/XMetaL/DLL/SaveDialog.h
+++ cdr/XMetaL/DLL/SaveDialog.h
@@ -1,3 +1,4 @@
+#include "afxwin.h"
 #if !defined(AFX_SAVEDIALOG_H__A7704207_A1DB_4D11_8325_B0876F2DA135__INCLUDED_)
 #define AFX_SAVEDIALOG_H__A7704207_A1DB_4D11_8325_B0876F2DA135__INCLUDED_
 
@@ -14,35 +15,41 @@ class CSaveDialog : public CDialog
 {
 // Construction
 public:
-	CSaveDialog(CWnd* pParent = NULL);
+    CSaveDialog(CWnd* pParent = NULL);
 
 // Dialog Data
-	//{{AFX_DATA(CSaveDialog)
-	enum { IDD = IDD_SAVE_DIALOG };
-	BOOL	m_validate;
-	BOOL	m_checkIn;
-	CString	m_comment;
-	BOOL	m_createVersion;
-	BOOL	m_versionPublishable;
-	BOOL	m_docInactive;
-	BOOL	m_readyForReview;
-	//}}AFX_DATA
+    //{{AFX_DATA(CSaveDialog)
+    enum { IDD = IDD_SAVE_DIALOG };
+    BOOL    m_validate;
+    BOOL    m_checkIn;
+    CString m_comment;
+    BOOL    m_createVersion;
+    BOOL    m_versionPublishable;
+    BOOL    m_docInactive;
+    BOOL    m_readyForReview;
+    //}}AFX_DATA
 
 
 // Overrides
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CSaveDialog)
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
-	//}}AFX_VIRTUAL
+    // ClassWizard generated virtual function overrides
+    //{{AFX_VIRTUAL(CSaveDialog)
+    protected:
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+    //}}AFX_VIRTUAL
 
 // Implementation
 protected:
 
-	// Generated message map functions
-	//{{AFX_MSG(CSaveDialog)
-	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
+    // Generated message map functions
+    //{{AFX_MSG(CSaveDialog)
+    //}}AFX_MSG
+    DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnBnClickedCheck4();
+    afx_msg void OnBnClickedCheck3();
+    afx_msg void OnBnClickedCheck1();
+private:
+    void fixCheckBoxes();
 };
 
 //{{AFX_INSERT_LOCATION}}
